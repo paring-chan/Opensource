6a78b64c2259a211dc6cec090de1c6c8d4005d6e
안꿀꿀 <63600304+MadeGOD@users.noreply.github.com>
2020-08-02 20:27:31 +0800

    유저 재보로 인해 패치

--- command/ADMIN/status.js
+++ command/ADMIN/status.js
@@ -1,13 +1,13 @@
 const os = require("os");
 module.exports.run = async (bot, message, args) => {
   message.channel.send(
-    `\`\`\`SERVER INFO-->\nCPU INFO -->\nCPU MODEL = ${os.cpus.model}\nCPU CLOCK SPEED = ${os.cpus.speed} MHz\nCPU IDLE TIME = ${os.cpus.idle} ms\n----------------------------------------\nMEMORY INFO -->\nTOTAL MEMORY = ${os.totalmem} MB\nFREE MEMORY = ${os.freemem} MB\`\`\``
+    `\`\`\`SERVER INFO-->\nCPU INFO -->\nCPU MODEL = ${os.cpus[0].model}\nCPU CLOCK SPEED = ${os.cpus[0].speed} MHz\nCPU IDLE TIME = ${os.cpus[0].idle} ms\n----------------------------------------\nMEMORY INFO -->\nTOTAL MEMORY = ${os.totalmem} MB\nFREE MEMORY = ${os.freemem} MB\`\`\``
   );
 };
 exports.config = {
   name: "시스템정보",
-  aliases: ["help", "bothelp"],
+  aliases: ["봇상태", "status"],
   category: ["ADMIN"],
-  des: ["도움말에 대한 설명"],
+  des: ["봇상태에 대한 설명"],
   use: ["ㄲ 시스템정보"]
 };
